d90c75d89fadd6f1be626b986850d0466e426ab0
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-13 23:29:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,4 +4,21 @@ class School
   def initialize(name)
     @name = name
     @roster = {}
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def add_student(name, grade)
+    roster[grade] << name
+  end
+  
+  def sort
+    sortedGrades = {}
+    roster.each { |grade, student|
+      sortedGrades[grade] = student.sort
+    }
+    return sortedGrades
+  end
 end
