4e2b79706ce82575d3a9552334bf93b21a65a969
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-13 23:13:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,3 @@
 class School
-  attr_accessor :name, :roster
-  
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
-  
-  def grade(grade)
-    roster[grade]
-  end
-  
-  def add_student(name, grade)
-    roster[grade] << name
-  end
-  
-  def sort 
-    returnArray = []
-    roster.each { |grade, student|
-      returnArray[grade] = student.sort
-    }
-    return returnArray
+  attr_accessor :name,
 end
