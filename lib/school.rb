09e6c5d9d37b03989f977f66e5709c9369862d88
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-13 23:36:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def add_student(name, grade)
+    roster[grade] || roster[grade] = []
+    roster[grade] << name
+  end
+  
+  def sort
+    sortedGrades = {}
+    roster.each { |grade, student|
+      sortedGrades[grade] = student.sort
+    }
+    return sortedGrades
+  end
+end
